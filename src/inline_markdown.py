e486f50add0c8721f52bca7e1ad41834b9f06b02
Mark Hamlin <mhamlin0621@proton.me>
2026-08-09 16:40:28 -0500

    Chapter 3: Lesson 5

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -27,3 +27,40 @@ def extract_markdown_images(text):
 def extract_markdown_links(text):
     matches = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)",text)
     return (matches)
+
+def split_nodes_image(oldnodes: list[TextNode]) -> list[TextNode]:
+    new_node_list = []
+    for node in oldnodes:
+        text = node.text
+        image_text = extract_markdown_images(text)
+        previous_split_text = node.text
+        for image_tuple in image_text:
+            alt_text,url = image_tuple
+            delimiter = f"![{alt_text}]({url})"
+            split_text = previous_split_text.split(delimiter,1)
+            if len(split_text[0]) != 0:
+                new_node_list.append(TextNode(split_text[0],TextType.TEXT))
+            new_node_list.append(TextNode(alt_text,TextType.IMAGE,url))
+            previous_split_text = split_text[1]
+        if len(previous_split_text) != 0:
+            new_node_list.append(TextNode(previous_split_text,TextType.TEXT))
+    return new_node_list
+
+
+def split_nodes_link(old_nodes: list[TextNode]) -> list [TextNode]:
+    new_node_list = []
+    for node in old_nodes:
+        text = node.text
+        image_text = extract_markdown_links(text)
+        previous_split_text = node.text
+        for image_tuple in image_text:
+            alt_text,url = image_tuple
+            delimiter = f"[{alt_text}]({url})"
+            split_text = previous_split_text.split(delimiter,1)
+            if len(split_text[0]) != 0:
+                new_node_list.append(TextNode(split_text[0],TextType.TEXT))
+            new_node_list.append(TextNode(alt_text,TextType.LINK,url))
+            previous_split_text = split_text[1]
+        if len(previous_split_text) != 0:
+            new_node_list.append(TextNode(previous_split_text,TextType.TEXT))
+    return new_node_list
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -94,7 +94,36 @@ class TestInlineMarkdown(unittest.TestCase):
         )
         self.assertListEqual([("to boot dev","https://www.boot.dev")],matches)
 
-
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode("second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"),
+            ],
+            new_nodes,
+        )
+    def test_split_links(self):
+        node = TextNode(
+            "This is text with an [This is an Image](https://i.imgur.com/zjjcJKZ.png) and another [This is the second Image](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("This is an Image", TextType.LINK,"https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode("This is the second Image",TextType.LINK,"https://i.imgur.com/3elNhQu.png")
+            ],
+            new_nodes,
+        )
 
 if __name__ == "__main__":
     unittest.main()
